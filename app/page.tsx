5be139e76db3f0cb11635da598eeec8081adc2c5
SUBHANKAR <mannasubhankar06@gmail.com>
2025-10-26 23:58:01 +0530

    Added Sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 import Image from 'next/image';
 import {useState} from 'react';
 import { assets } from "@/assets/assets";
+import Sidebar from '@/components/Sidebar';
 
 export default function Home() {
 
@@ -14,12 +15,27 @@ export default function Home() {
     <div>
       <div className = "flex h-screen">
           {/* --sidebar-- */}
+          <Sidebar expand = {expand} setExpand = {setExpand}/>
         <div className='flex-1 flex flex-col items-center justify-center px-4 pb-8 bg-[#292a2d] text-white relative' >
             <div className='md:hidden absolute px-4 top-6 flex items-center justify-between w-full'>
               <Image onClick={() => (expand ? setExpand(false) : setExpand(true))}
                className = "rotate-180" src = {assets.menu_icon} alt="Menu icon" />
               <Image className = "opacity-70" src = {assets.chat_icon} alt="Chat icon"/>
             </div>
+            {message.length === 0 ? (
+              <>
+                <div className='flex items-center gap-3'>
+                  <Image src = {assets.logo_icon} alt="Logo icon" className = "h-16"/>
+                  <p className = "text-2xl font-medium">Hi, I'm Deepseek Clone.</p>
+                </div>
+                <p className='text-sm mt-2'>How can I help you today?</p>
+              </>
+            ) : (
+              <div></div>
+            )}
+            {/* prompt box */}
+            <p className='text-xs absolute bottom-1 text-gray-500'>AI generated responses are property of Deepseek.</p>
+
           </div>
       </div>
     </div>
---
+++ components/Sidebar.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Image from 'next/image';
+import { assets } from "@/assets/assets";
+
+const Sidebar = ({expand, setExpand}) => {
+  return (
+    <div className={`flex flex-col justify-between bg-[#212327] pt-7 transition-all z-50 max-md:absolute max-md:h-screen ${expand ? 'px-4 w-64' : 'md:w-20 w-0 max-md:overflow-hioidden'}`}>
+      <div>
+        <div className= {`flex ${expand ? 'flex-row gap-10' : 'flex-col items-center gap-8'}`}>
+            <Image src = {expand ? assets.logo_text : assets.logo_icon} alt="Logo icon" className = {expand ? "w-36" : "w-10"}/>
+
+            <div onClick={()=> expand ? setExpand(false) : setExpand(true)}
+            className='group relative flex items-center justify-center hover:bg-gray-500/20 transition-all duration-300 h-9 w-9 aspect-square rounded-lg cursor-pointer'>
+            <Image src = {assets.menu_icon} alt="Menu icon" className = 'md:hidden'/>
+            <Image src = {expand ? assets.sidebar_close_icon : assets.sidebar_icon} alt="Sidebar icon" className = 'hidden md:block w-7'/>
+
+            <div className= {`absolute w-max ${expand ? 'left-1/2 -translate-x-1/2 top-12' : '-top-12 left-0'} opacity-0 group-hover:opacity-100 transition bg-black text-white text-sm px-3 py-2 rounded-lg shadow-lg pointer-events-none`}>
+              {expand ? 'Close Sidebar' : 'Open Sidebar'}
+
+              <div className= {`w-3 h-3 absolute bg-black rotate-45 ${expand ? 'left-1/2 -top-1.5 -translate-x-1/2' : 'left-4 -bottom-1.5'}`}>
+              </div>
+            </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
